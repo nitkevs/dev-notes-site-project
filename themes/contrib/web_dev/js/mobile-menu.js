230f09a1dfe861b3a253d725de5591002a3f9c51
nikevs <nitkevs@gmail.com>
2020-08-26 18:24:01 +0300

    Установил модуль captcha и обновил тему до 1.3.0

--- themes/contrib/web_dev/js/mobile-menu.js
+++ themes/contrib/web_dev/js/mobile-menu.js
@@ -1,24 +1,27 @@
-let items = document.querySelectorAll('#sidebar-first ul.menu a, #sidebar-first .item-list a, #sidebar-first .h2-container h2, #sidebar-first .tagclouds-term a');
+let items;
 let menu = document.getElementById('sidebar-first');
 let openMenuButton = document.getElementById('open-menu-button');
 let closeMenuButton = document.getElementById('close-menu-button');
 openMenuButton.addEventListener('click', function() {
-    menu.style.opacity = "1";
-    menu.style.zIndex = "3";
+    menu.style.opacity = '1';
+    menu.style.zIndex = '3';
+    items = document.querySelectorAll('#sidebar-first ul.menu a, #sidebar-first .field-content a, #sidebar-first .h2-container h2, #sidebar-first .tagclouds-term a');
   for (i = 0; i < items.length; i++) {
-    items[i].style.transitionDelay = i/50  + "s";
-    items[i].style.transform = "translateX(0%)";
+    items[i].style.transitionProperty = 'transform';
+    items[i].style.transitionDelay = i/50  + 's';
+    items[i].style.transform = 'translateX(0%)';
   }
 });
 
 closeMenuButton.addEventListener('click', function() {
-   menu.style.opacity = "0";
+   menu.style.opacity = '0';
    setTimeout(function() {
      menu.style.zIndex = '0';
      for (i = 0; i < items.length; i++) {
-     items[i].style.transform = "translateX(-100%)";
+     items[i].style.transitionProperty = 'none';
+     items[i].style.transform = 'translateX(-100%)';
      }
-   }, 600);
+   }, 300);
 });
 
 
